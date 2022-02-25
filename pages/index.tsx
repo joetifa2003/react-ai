e3065316c870b61d25ad3b22ab33951ec5a5a0ea
joetifa2003 <joetifa2003@gmail.com>
2022-02-25 02:21:43 +0200

    fix: cleaning up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,5 @@
 const Home = () => {
-    return (
-        <div className="">
-            <div className="text-9xl">Hello World!</div>
-        </div>
-    );
+    return <div className=""></div>;
 };
 
 export default Home;
